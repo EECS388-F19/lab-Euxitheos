e89d1a218a09f60f1f11801536a5ad69f854c4cf
Euxitheos <thejakej@umich.edu>
2019-09-09 15:30:22 -0400

    helloworld

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import random
+
+print("Jake")
+x = random.randrange(100)
+y = random.randrange(100)
+print(x)
+print(y)
+
+sum = x + y
+avg = sum / 2
+
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
